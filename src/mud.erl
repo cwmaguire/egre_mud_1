6b3f8949bbfa715a300139807ad445214d1ef80f
Chris Maguire <cwmaguire@protonmail.com>
2023-08-06 21:20:01 -0700

    Remove unrelated GERLSHMUD sources
    A lot of GERLSHMUD source files were split into the graph and MUD engine
    projects (EGRE and EGRE_MUD).
    
    In EGRE_MUD_1 I only need the files necessary to support the rules for
    the reference MUD. I don't need anything to do with the graph or the MUD
    engine.

--- src/mud.erl
+++
@@ -1,7 +0,0 @@
-%% Copyright 2022, Chris Maguire <cwmaguire@protonmail.com>
--module(gerlshmud).
-
--export([create/2]).
-
-create(Type, Props) ->
-    gerlshmud_object:start_link('TODO_add_object_id', Type, Type:create(Props)).
--- src/mud_handler.erl
+++
@@ -1,6 +0,0 @@
-%% Copyright 2022, Chris Maguire <cwmaguire@protonmail.com>
--module(gerlshmud_handler).
-
--callback attempt({pid(), list(), tuple()}) -> any().
--callback succeed({pid(), tuple()}) -> list().
--callback fail({pid(), atom, tuple()}) -> list().
